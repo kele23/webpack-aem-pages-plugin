423f5dc114d2b3c8984b9975eb89674a90dc23b2
Michele Scala <m.scala@reply.it>
2022-04-07 17:21:11 +0200

    Fix

--- libs/JsUseProvider.js
+++ libs/JsUseProvider.js
@@ -1,21 +1,27 @@
 const fs = require('fs');
 const vm = require('vm');
-
-module.exports = function (path) {
+const path = require('path');
+module.exports = function (mpath) {
     return function () {
-        this.path = path;
+        this.mpath = mpath;
         this.use = async function (globals) {
-            if (!fs.existsSync(this.path)) return {};
-
-            const source = fs.readFileSync(this.path, { encoding: 'utf-8' });
+            if (!fs.existsSync(this.mpath)) return {};
+            const _require = require;
+            const source = fs.readFileSync(this.mpath, { encoding: 'utf-8' });
             const vmContext = vm.createContext({
                 use: (fn) => {
                     return fn.call(globals);
                 },
-                console: console,
-                require: require
+                console,
+                exports,
+                require: (filepath) => {
+                    const p = path.resolve(path.dirname(this.mpath), filepath);
+                    return _require(p);
+                },
+                module,
+                __filename,
+                __dirname,
             });
-
             const vmScript = new vm.Script(source);
             return vmScript.runInContext(vmContext);
         };
